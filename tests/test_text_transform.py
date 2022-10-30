9c020c2c2af689a9ab7781ccec32aa4eb65c0324
Jarbton <j.barton.mail@googlemail.com>
2022-10-30 22:51:43 +0000

    Add failing UTs and e2e boilerplate
    text_transform workflow

--- tests/test_text_transform.py
+++ tests/test_text_transform.py
@@ -1,4 +1,4 @@
-from pytest import mark
+from pytest import mark, param
 
 from workflows.text_transform.src.text_transform import spongebob_case
 
@@ -18,6 +18,8 @@ from workflows.text_transform.src.text_transform import spongebob_case
             "HELLO WORLD",
             "hElLo wOrLd",
         ),
+        param("hello-world", "hElLo wOrLd", marks=mark.xfail),
+        param("hello_world", "hElLo wOrLd", marks=mark.xfail),
     ],
 )
 def test_spongebob_case(input_string: str, expected: str):
@@ -29,3 +31,14 @@ def test_spongebob_case(input_string: str, expected: str):
         expected: The expected output of the spongebob_case function.
     """
     assert spongebob_case(input_string) == expected
+
+
+def test_e2e_text_transformation():
+    """
+    Test the text transform feature works end to end.
+    Passes input string as argv and expects JSON output.
+
+    args:
+
+    """
+    assert True
