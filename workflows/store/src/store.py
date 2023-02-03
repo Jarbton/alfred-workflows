8b561f11dacbdc36c105af57160725329464594d
Jarbton <j.barton.mail@googlemail.com>
2023-02-03 01:25:32 +0000

    Fix check for existing URLs

--- workflows/store/src/store.py
+++ workflows/store/src/store.py
@@ -20,16 +20,17 @@ def main():
 
     keyword, url = args
 
-    # Check if keyword is already in store
-    store = load_store()
-    if keyword in store:
-        print("Keyword already in store", file=stderr)
-        print("Keyword is already in the store!")
-        return
-
     # Check if url is valid and add http if not
     if not url.startswith("http"):
         url = "http://" + url
+
+    # Check if keyword is already in store
+    store = load_store()
+    for item in store:
+        if item.url == url:
+            print("URL already in store", file=stderr)
+            print(f"URL is already in the store with keyword {item.keyword}", end="")
+            return
 
     # Add keyword and url to store
     item = StoreItem(keyword, url)
